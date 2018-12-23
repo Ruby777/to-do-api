d4abaea5c8c75692cd798700265e20ce55c3cdc3
Lucy <lucynn11@gmail.com>
2018-12-23 21:31:42 +0400

    tdd for controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 Rails.application.routes.draw do
 
-  resources :lists do
-    resources :items, except: [:index]
-  end
-  
   namespace :api, defaults: { format: :json } do
-    resources :users
+
+    resources :users do
+      resources :lists
+    end
+
+    resources :lists, only: [] do
+      resources :items, only: [:create]
+    end
+
+    resources :items, only: [:destroy]
   end
 
   get 'welcome/index'
